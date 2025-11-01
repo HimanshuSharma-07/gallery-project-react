ef6dccbd23405a42a4d40a2b66246fbdfb0bb9d1
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-11-01 22:32:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+  const [userData, setUserData] = useState([])
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async ()=>{
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=30`)
+    setUserData(response.data)
+    
+    setUserData(response.data )
+
+  }
+  
+  useEffect(function(){
+    getData()
+  },[index])
+  
+  let printUserData = <h3 className='text-gray-400 text-xs'>User data is not available</h3>
+
+  if (userData.length > 0){
+    printUserData = userData.map(function(elem, idx){
+      return <div key={idx}>
+
+        <a href={elem.url} target='_blank'>
+
+          <div className=" h-40 w-50 overflow-hidden rounded-xl">
+          <img className='h-full w-full  object-cover 'src={elem.download_url} />
+
+        </div>
+        <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
+
+      </div>
+    })
+
+
+  }
+
+
+  return (
+    <div className='bg-black overflow-auto h-screen py-4  text-white' >
+      
+
+    <h1>{index}</h1>
+
+    <div className='flex flex-wrap gap-3 p-2'>{printUserData}</div>
+
+
+
+    <div className='flex justify-center gap-6 items-center p-4'>
+      <button onClick={()=>{
+        if(index > 1){
+          setIndex(index-1)
+        }
+      }}
+      className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'>
+        Prev
+        </button>
+
+      <button onClick={()=>{
+        setIndex(index+1)
+      }} 
+      className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'>
+        Next
+        </button>
+        
+    </div>
+      
+    </div>
+    
+  )
+}
+
+export default App
